2ec186a56bdc0d69f7efd2393139979a9a5cb03f
Christina <christinaraganit@gmail.com>
2023-11-28 06:31:24 -0800

    Added icon links to about me page

--- portfolio/components/button.tsx
+++ portfolio/components/button.tsx
@@ -1,19 +1,34 @@
 interface Props {
   text: string;
   link: string;
+  isPrimary?: boolean;
 }
 
 export default function Button(props: Props) {
-  return (
-    <a
-      href={props.link}
-      className="group-hover/button block w-fit py-[0.0625rem] pt-[0.0625rem] pb-0 rounded-lg 
+  if (!props.isPrimary) {
+    return (
+      <a
+        href={props.link}
+        className="group-hover/button block w-fit py-[0.0625rem] pt-[0.0625rem] pb-0 rounded-lg 
       bg-gradient-to-r from-neutral-900 via-neutral-500/60 to-neutral-900
-      hover/button:from-neutral-800/60 hover/button:via-neutral-500 hover/button:to-neutral-800/60 hover/button:cursor-pointer border border-neutral-700/30"
-    >
-      <div className="text-neutral-300/90 font-medium tracking-tight block px-4 py-2 bg-neutral-800/90 hover/button:bg-neutral-800/60 hover/button:cursor-pointer rounded-md transition-colors">
-        {props.text}
-      </div>
-    </a>
-  );
+      hover/button:from-neutral-800/60 hover/button:via-neutral-500 hover/button:to-neutral-800/60 hover/button:cursor-pointer border-l border-r border-neutral-700/[18%]"
+      >
+        <div className="text-neutral-300/90 font-medium tracking-tight block px-4 py-2 bg-neutral-800/90 hover/button:bg-neutral-800/60 hover/button:cursor-pointer rounded-md transition-colors">
+          {props.text}
+        </div>
+      </a>
+    );
+  } else {
+    return (
+      <a
+        href={props.link}
+        className="group-hover/button block w-fit py-[0.0625rem] pt-[0.0625rem] pb-0 rounded-lg 
+                  bg-gradient-to-r from-neutral-800/60 via-teal-500 to-neutral-800/60 hover/button:cursor-pointer hover/button:via-teal-400"
+      >
+        <div className="text-teal-50 tracking-tight block px-4 py-2 bg-teal-800/60 hover/button:bg-teal-800/60 hover/button:cursor-pointer rounded-md transition-colors hover/button:shadow-lg hover/button:shadow-teal-400/[18%] transition-shadow">
+          {props.text}
+        </div>
+      </a>
+    );
+  }
 }
--- portfolio/components/footer.tsx
+++ portfolio/components/footer.tsx
@@ -3,9 +3,11 @@ export default function Footer() {
     <footer className="bg-neutral-900/60 w-full flex flex-col items-center mt-40">
       <div className="divider"></div>
       <div className="p-6 lg:px-20 w-full max-w-[80rem] grid md:grid-cols-2 lg:gap-8">
-        <p className="max-w-full md:max-w-sm text-sm text-center md:text-left text-neutral-400 order-last md:order-first pb-0">
-          Crafted with Tailwind and NextJS by Christina Raganit.
-        </p>
+        <div>
+          <p className="max-w-full md:max-w-sm text-sm text-center md:text-left text-neutral-400 order-last md:order-first pb-0">
+            Crafted with Tailwind and NextJS by Christina Raganit.
+          </p>
+        </div>
         <div>
           <nav className="flex flex-col md:flex-row items-center h-full w-full gap-4 justify-end pb-6 md:pb-0">
             <a href="/" className="link">
